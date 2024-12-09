37bbf14fa8a92a5198101de51afd284fd902c291
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-12-09 16:29:36 +0100

    build: Use .gitignore for eslint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,16 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
+    includeIgnoreFile(gitignorePath),
     js.configs.recommended,
     eslintConfigPrettier,
     {
